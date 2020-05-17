a4aa8dd52c963dbe6721857cffdeadc5e9183ae8
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-05-18 00:47:04 +0900

    adjustment of image size setting

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [800, 800]
+    # process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
@@ -44,4 +44,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  process resize_to_limit: [800, 800]
 end
