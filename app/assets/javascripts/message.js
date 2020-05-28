fc64a074f97ec083a7ba6e1064d629c7edfa8a70
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-05-28 18:42:28 +0900

    add overflow scroll

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,6 +63,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
       $('.submit-btn').prop('disabled', false);
     })
