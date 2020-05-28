57743019a308c5273a48cc5ece9fbe11efb40a45
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-05-28 18:11:51 +0900

    receive json at done method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,53 @@
 $(function(){
+  function buildHTML(message){
+    if (message.text && message.image) {
+      var html =
+      `
+      <div class="message">
+        <div class="message__upper-info">
+          <p class="message__upper-info__talker">${message.user_name}</p>
+          <p class="message__upper-info__date">${message.created_at}</p>
+        </div>
+        <p class="message__text">${message.text}</p>
+      </div>
+      <div class="message>
+        <div class="message__upper-info">
+          <p class="message__upper-info__talker">${message.user_name}</p>
+          <p class="message__upper-info__date">${message.created_at}</p>
+        </div>
+        <img class="message__image">${message.image}</p>
+      </div>
+      `
+    }
+    else if (message.text) {
+      var html =
+      `
+      <div class="message">
+        <div class="message__upper-info">
+          <p class="message__upper-info__talker">${message.user_name}</p>
+          <p class="message__upper-info__date">${message.created_at}</p>
+        </div>
+        <p class="message__text">${message.text}</p>
+      </div>
+      `
+    }
+    else if (message.image) {
+      var html =
+      `
+      <div class="message>
+        <div class="message__upper-info">
+          <p class="message__upper-info__talker">${message.user_name}</p>
+          <p class="message__upper-info__date">${message.created_at}</p>
+        </div>
+        <img class="message__image">${message.image}</p>
+      </div>
+      `
+    }
+    else {
+      var html = ``
+    }
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +59,9 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
     })
   });
 });
