a00ef94790e701c6ee1eea91137793349a9f8c55
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-05-30 11:50:43 +0900

    add custom data property to html of message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ $(function(){
     if (message.image) {
       html =
       `
-      <div class="message">
+      <div class="message" data-message-id=${message.id}>
         <div class="message__upper-info">
           <p class="message__upper-info__talker">${message.user_name}</p>
           <p class="message__upper-info__date">${message.created_at}</p>
@@ -17,7 +17,7 @@ $(function(){
     else {
       html =
       `
-      <div class="message">
+      <div class="message" data-message-id=${message.id}>
         <div class="message__upper-info">
           <p class="message__upper-info__talker">${message.user_name}</p>
           <p class="message__upper-info__date">${message.created_at}</p>
@@ -55,15 +55,10 @@ $(function(){
   });
 
   let reloadMessages = function() {
-    // カスタムデータ属性を利用し
-    // ブラウザに表示されている
-    // 最新メッセージのidを取得
     let last_message_id = $('.message:last').data("message-id");
     $.ajax({
-      // ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'GET',
       dataType: 'json',
-      // dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
