a07f6ff4b5d219653ad239a9064ef22713a9b5c7
osawa4017 <br0wn.ke1.4017@gmail.com>
2020-05-30 11:46:28 +0900

    add reloadMessages to message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,4 +53,24 @@ $(function(){
       $('.submit-btn').prop('disabled', false);
     });
   });
+
+  let reloadMessages = function() {
+    // カスタムデータ属性を利用し
+    // ブラウザに表示されている
+    // 最新メッセージのidを取得
+    let last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      // ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'GET',
+      dataType: 'json',
+      // dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
 });
